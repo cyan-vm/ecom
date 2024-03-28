017d7b3f9ed6ab7300eca83ccbe563f62236aeb2
cyan-vm <cyan.mv@gmail.com>
2024-03-27 22:26:14 -0600

    Add Product To Cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,17 +1,25 @@
 # from store.models import Product, Profile
+
 class Cart():
-	def __init__(self, request):
-		self.session = request.session
-		# Get request
-		self.request = request
-		# Get the current session key if it exists
-		cart = self.session.get('session_key')
+  def __init__(self, request):
+    self.session = request.session
+    # print(self.session)
+    # Get request
+    self.request = request
+    # Get the current session key if it exists
+    cart = self.session.get('session_key')
+    # If the user is new, no session key!  Create one!
+    if 'session_key' not in request.session:
+      cart = self.session['session_key'] = {}
+    # Make sure cart is available on all pages of site
+    self.cart = cart
 
-		# If the user is new, no session key!  Create one!
-		if 'session_key' not in request.session:
-			cart = self.session['session_key'] = {}
-
-		# Make sure cart is available on all pages of site
-		self.cart = cart
-
-
+  def add(self, product):
+    product_id = str(product.id)
+    # Logic
+    if product_id in self.cart:
+      pass
+    else:
+      self.cart[product_id] = {'price': str(product.price)}
+      # self.cart[product_id] = int(product_qty)
+    self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,18 +1,38 @@
 # from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
-# from .cart import Cart
-# from store.models import Product
-# from django.http import JsonResponse
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 # from django.contrib import messages
 
 def cart_summary(request):
-	# # Get the cart
-	# cart = Cart(request)
-	# cart_products = cart.get_prods
-	# quantities = cart.get_quants
-	# totals = cart.cart_total()
-	return render(request, "cart_summary.html", {})
+  # # Get the cart
+  # cart = Cart(request)
+  # cart_products = cart.get_prods
+  # quantities = cart.get_quants
+  # totals = cart.cart_total()
+  return render(request, "cart_summary.html", {})
 
+def cart_add(request):
+  # Get the cart
+  cart = Cart(request)
+  # test for POST
+  # print(f"Cart session : {cart.session} Cart request {cart.request} Cart session key : {cart.cart}")
+  if request.POST.get('action') == 'post':
+    # Get stuff
+    product_id = int(request.POST.get('product_id'))
+    # product_qty = int(request.POST.get('product_qty'))
+    # lookup product in DB
+    product = get_object_or_404(Product, id=product_id)
+    # Save to session
+    cart.add(product=product)
+    # Get Cart Quantity
+    # cart_quantity = cart.__len__()
+    # Return resonse
+    # response = JsonResponse({'Product Name: ': product.name})
+    response = JsonResponse({'Product Name': product.name})
+    # messages.success(request, ("Product Added To Cart..."))
+    return response 
 
 
 # Create your views here.
